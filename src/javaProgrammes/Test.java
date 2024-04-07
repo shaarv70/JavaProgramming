4f2ce659e4fe1a23fc52686ab18d215834552091
Arvind Sharma <arvindsharma50480@gmail.com>
2024-04-07 15:13:19 +0530

    clean

--- src/javaProgrammes/Test.java
+++ src/javaProgrammes/Test.java
@@ -7,31 +7,7 @@ public class Test {
 
     public static void main(String[] args)  {
     	
-    	String expr = "{[(}])";
-    	int x=90;
-        int k=64;
-        int y=456;
-
-        int l= 456;
-<<<<<<< HEAD
-	    int rusting=8;
-=======
-	int rusting=8;
->>>>>>> refs/heads/master
-        int cast=90;
-        int size=67;
-        
-       String h="Casttypes";
-       String l1="types-1";
-<<<<<<< HEAD
-       
-       Map<String,String> mymap= new HashMap<String,String>();
-       mymap.put("Aa","aa");
-
-=======
-	    String z="priyanka";
-     String o="89";
->>>>>>> refs/heads/master
+    
 
     
     }
