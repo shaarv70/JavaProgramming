330d8d34864508d64fda5a9cfa1ee052f88aa5ca
arvind <arvindsharma50480@gmail.com>
2024-11-09 12:59:05 +0530

    Changes

--- src/javaProgrammes/Test.java
+++ src/javaProgrammes/Test.java
@@ -1,15 +1,12 @@
 package javaProgrammes;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
 
     public static void main(String[] args)  {
     	
     int l=45;
     int x=109;
-    int x=20;
+    
     int z=10;
     String g="arvind";
     String o="12";
